0677699041283b4d8f5b0de95edd5b744f918a46
Nkowne63 <neutron63zf@gmail.com>
2018-09-02 13:20:10 +0900

    esdsdfc

--- routes/additionals.js
+++ routes/additionals.js
@@ -4,7 +4,7 @@ const Page = reqlib('models/page')
 module.exports = router => {
     router.get('/n_ranking', async (req, res) => {
         const r = await axios.get('http://localhost:3001/get')
-        const array = r.data
+        const array = r.data.rank
 
         const pages = await Page.getListByIds(array)
         // -> : Pages
