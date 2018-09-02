c6dc5f9c7a559b77f9e0a562a754433f516709d7
Nkowne63 <neutron63zf@gmail.com>
2018-09-02 11:00:57 +0900

    change db

--- database/index.js
+++ database/index.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql')
 
 const comicdb = mysql.createConnection({
-    host: 'us-cdbr-iron-east-01.cleardb.net',
-    user: 'bb72568e1ffe6c',
-    password: '9c7cce5f',
-    database: 'heroku_5a61d935653267e'
+    host: 'localhost',
+    user: 'root',
+    password: 'nagisac2018',
+    database: 'comicdb'
 })
 
 const handleDisconnect = () => {
